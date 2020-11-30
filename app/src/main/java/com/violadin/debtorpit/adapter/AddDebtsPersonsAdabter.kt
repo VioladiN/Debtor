d0bfeed8f3324c9e96b63487cbd6ff04db4e5797
VioladiN <violadin.kisel@mail.ru>
2020-11-30 21:35:32 +0300

    Finally added adding debts for AddDebtsFragment.kt

--- app/src/main/java/com/violadin/debtorpit/adapter/AddDebtsPersonsAdabter.kt
+++ app/src/main/java/com/violadin/debtorpit/adapter/AddDebtsPersonsAdabter.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.violadin.debtorpit.R
@@ -11,6 +12,8 @@ import com.violadin.debtorpit.model.Person
 
 class AddDebtsPersonsAdapter(private var persons: List<Person>):
         RecyclerView.Adapter<AddDebtsPersonsAdapter.ViewHolder>() {
+
+    val checked = BooleanArray(persons.size)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).
@@ -27,11 +30,18 @@ class AddDebtsPersonsAdapter(private var persons: List<Person>):
         holder.name.text = persons[position].firstName.toString() +
                 " " + persons[position].lastName.toString()
         holder.debt.text = persons[position].debt.toString()
+
+        holder.checkBox.setOnClickListener {
+            checked[position] = !checked[position]
+        }
     }
+
+    fun getCheckedPersons(): BooleanArray = checked
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         val name = itemView.findViewById<TextView>(R.id.tv_name)
         val debt = itemView.findViewById<TextView>(R.id.tv_dept)
+        val checkBox = itemView.findViewById<CheckBox>(R.id.cb_selecting)
     }
 }
--- app/src/main/java/com/violadin/debtorpit/fragments/AddDebtsFragment.kt
+++ app/src/main/java/com/violadin/debtorpit/fragments/AddDebtsFragment.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +18,8 @@ import com.violadin.debtorpit.R
 import com.violadin.debtorpit.adapter.AddDebtsPersonsAdapter
 import com.violadin.debtorpit.adapter.ListOfPersonsAdapter
 import com.violadin.debtorpit.viewmodel.PersonViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class AddDebtsFragment: Fragment() {
 
@@ -24,16 +31,56 @@ class AddDebtsFragment: Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?):
+            View? {
         val view = inflater.inflate(R.layout.adddebts_activity, container, false)
         model = ViewModelProvider(this).get(PersonViewModel::class.java)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.list_add_debt)
-        val linearLayoutManager = LinearLayoutManager(view.context, RecyclerView.VERTICAL, false)
+        val linearLayoutManager = LinearLayoutManager(view.context,
+                RecyclerView.VERTICAL, false)
         recyclerView.layoutManager = linearLayoutManager
-        model.allPersons.observe(viewLifecycleOwner, Observer {
-            persons -> recyclerView.adapter = AddDebtsPersonsAdapter(persons)
+
+        var checked: BooleanArray? = null
+        model.allPersons.observe(viewLifecycleOwner, {
+            persons ->
+            run {
+                val adapter = AddDebtsPersonsAdapter(persons)
+                checked = adapter.getCheckedPersons()
+                recyclerView.adapter = adapter
+            }
         })
+
+
+        val editText = view.findViewById<EditText>(R.id.debts_et)
+        val meTooCheckBox = view.findViewById<CheckBox>(R.id.cb_me_too)
+        val addDebtButton = view.findViewById<Button>(R.id.add_depts)
+        addDebtButton.setOnClickListener {
+
+            var countOfCheckedPersons = 0
+            for (item in checked!!) {
+                if (item)
+                    ++countOfCheckedPersons
+            }
+            if (meTooCheckBox.isChecked) ++countOfCheckedPersons
+            val debt = editText.text.toString().toDouble() / countOfCheckedPersons
+
+            for (i in checked!!.indices) {
+                if (checked?.get(i) == true) {
+                    val newDebt = debt + model.allPersons.value!![i].debt!!
+                    GlobalScope.launch {
+                        model.allPersons.value?.let { it1 -> model.update(it1[i], newDebt) }
+                    }
+                }
+            }
+
+            Toast.makeText(view.context,
+                    "Debts added successfully", Toast.LENGTH_LONG).show()
+        }
+
+
         return view
     }
 }
