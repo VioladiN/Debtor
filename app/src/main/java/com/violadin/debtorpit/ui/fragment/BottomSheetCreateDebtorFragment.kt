f850889dbe0b492239fae760bfe375c700fd7258
VioladiN <violadin.kisel@mail.ru>
2021-10-07 13:46:35 +0300

    redesign bottom sheet fragment for creating debtor

--- app/src/main/java/com/violadin/debtorpit/ui/fragment/BottomSheetCreateDebtorFragment.kt
+++ app/src/main/java/com/violadin/debtorpit/ui/fragment/BottomSheetCreateDebtorFragment.kt
@@ -1,13 +1,20 @@
 package com.violadin.debtorpit.ui.fragment
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.violadin.debtorpit.R
 import com.violadin.debtorpit.domain.model.Person
@@ -27,6 +34,15 @@ class BottomSheetCreateDebtorFragment(val viewModel: PersonViewModel): BottomShe
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.bottom_sheet_create_debtor_fragment, container, false)
+        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        dialog?.setOnShowListener {
+            Handler().postDelayed({
+                val d = dialog as BottomSheetDialog
+                val bottomSheet = d.findViewById<FrameLayout>(R.id.design_bottom_sheet)
+                val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet!!)
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }, 0)
+        }
         return view
     }
 
@@ -34,18 +50,6 @@ class BottomSheetCreateDebtorFragment(val viewModel: PersonViewModel): BottomShe
         super.onViewCreated(view, savedInstanceState)
 
         create_debtor.setOnClickListener {
-            it.apply { isEnabled = false; postDelayed({isEnabled = true}, 1000) }
-            addPerson(
-                Person(
-                    firstName = edit_first_name.text.toString(),
-                    lastName = edit_last_name.text.toString(),
-                    debt = if (edit_debt.text.isEmpty()) 0.0 else edit_debt.text.toString().toDouble(),
-                    phone = edit_phone.text.toString()
-                )
-            )
-        }
-
-        tv_add.setOnClickListener {
             it.apply { isEnabled = false; postDelayed({isEnabled = true}, 1000) }
             addPerson(
                 Person(
